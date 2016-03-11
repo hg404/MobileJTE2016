87f77faeb213e8356da58fd525d0efa8408dc603
Muhammad Rizki Juanda <muhammadrizkijuanda@gmail.com>
2016-03-11 20:00:25 +0700

    Tugas 06-Joystick

---
+++ Tugas06-Joystick/app/src/main/java/com/example/mrizkijuanda/tugas06_joystick/fragmentbawah.java
@@ -0,0 +1,22 @@
+package com.example.mrizkijuanda.tugas06_joystick;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class fragmentbawah extends Fragment {
+
+    public fragmentbawah() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bawah, container, false);
+    }
+}
